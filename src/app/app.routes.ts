c538abcced43063ec70da6bdfa5d04491d4165e5
Catalina Cano <catalinacano08@hotmail.com>
2018-01-26 17:07:09 -0500

    Limpieza de los routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,7 @@
 import { RouterModule, Routes } from '@angular/router';
-import { InicioComponent } from './components/pages/inicio/inicio.component';
+
 import { LoginComponent } from './components/login/login.component';
-import { RegistrarComponent } from './components/pages/registrar/registrar.component';
-import { AdministradorComponent } from './components/pages/administrador/administrador.component';
-import { AlojamientoComponent } from './components/pages/alojamiento/alojamiento.component';
-import { GaleriaComponent } from './components/pages/galeria/galeria.component';
-import { PerfilComponent } from './components/pages/perfil/perfil.component';
-import { patch } from 'webdriver-js-extender';
 import { NotpagefoundComponent } from './components/shared/notpagefound/notpagefound.component';
-import { PagesComponent } from './components/pages/pages.component';
-
-
-
 
 const appRoutes: Routes = [
     { path: 'login', component: LoginComponent },
