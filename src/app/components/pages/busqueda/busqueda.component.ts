12ab130d1638a0d670912d4c452796024fa81b2e
Catalina Cano <catalinacano08@hotmail.com>
2018-03-12 11:52:21 -0500

    Se realiza la busqueda y se ve en una tabla

--- src/app/components/pages/busqueda/busqueda.component.ts
+++ src/app/components/pages/busqueda/busqueda.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { URL_SERVICIOS } from '../../../config/config';
+import { AlojamientoConsulta } from '../../../models/alojamientoConsulta.model';
 
 @Component({
   selector: 'app-busqueda',
@@ -10,13 +11,17 @@ import { URL_SERVICIOS } from '../../../config/config';
 })
 export class BusquedaComponent implements OnInit {
 
+  alojamientos: AlojamientoConsulta[] = [];
+  estudiantes: any[] = [];
+  // sedes: any[] = [];
+
   constructor(
       public ativatedRoute: ActivatedRoute,
       public http: HttpClient
     ) {
       ativatedRoute.params.subscribe( params => {
         let termino = params['termino'];
-        console.log(termino);
+        this.buscar(termino);
       });
    }
 
@@ -25,7 +30,11 @@ export class BusquedaComponent implements OnInit {
 
   buscar(termino: string) {
     let url = URL_SERVICIOS + '/busqueda/todo/' + termino;
-    this.http.get(url);
+    this.http.get(url)
+            .subscribe((resp: any) => {
+              this.alojamientos = resp.alojamientos;
+              this.estudiantes = resp.estudiantes;
+            });
 
   }
 
